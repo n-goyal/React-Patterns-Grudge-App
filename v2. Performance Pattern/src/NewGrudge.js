c3056c097de5b1e08a8ca7a8c7ef588de37333ad
Nitin Goyal <goyalnitin634@gmail.com>
2020-11-29 08:55:15 +0530

    added performance optimization techniques

---
+++ v2. Performance Pattern/src/NewGrudge.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const NewGrudge = React.memo(({ onSubmit }) => {
+  console.log('Rendering New Grudge');
+  const [person, setPerson] = useState('');
+  const [reason, setReason] = useState('');
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    onSubmit({ person, reason });
+  };
+
+  return (
+    <form className="NewGrudge" onSubmit={handleChange}>
+      <input
+        className="NewGrudge-input"
+        placeholder="Person"
+        type="text"
+        value={person}
+        onChange={(event) => setPerson(event.target.value)}
+      />
+      <input
+        className="NewGrudge-input"
+        placeholder="Reason"
+        type="text"
+        value={reason}
+        onChange={(event) => setReason(event.target.value)}
+      />
+      <input className="NewGrudge-submit button" type="submit" />
+    </form>
+  );
+});
+
+export default NewGrudge;
